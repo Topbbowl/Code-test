20dd97cd9352179827f0505a75fa6cc61a5ac00e
大碗 <2541103109@qq.com>
2022-11-01 23:45:49 +0800

    11月1日测试

--- test_11_1.c/test_11_1.c/test11_1.c
+++ test_11_1.c/test_11_1.c/test11_1.c
@@ -1,1 +1,88 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int max(int x, int y)
+//
+//{
+//
+//	int z;
+//
+//	if (x > y) {
+//
+//		z = x;
+//
+//	}
+//
+//	else {
+//
+//		z = y;
+//
+//	}
+//
+//	return z;
+//
+//}
+//
+//int main()
+//
+//{
+//
+//	int x = 0;
+//
+//	int y = 0;
+//
+//	scanf("%d %d", &x, &y);
+//
+//	int z = max(x, y);
+//
+//	printf("%d ", z);
+//
+//	return 0;
+//
+//}
+//void test()
+//{
+//	int a = 1; 区别为int a前有无static！！！         
+//	a++;
+//	printf("%d ", a);
+//}
+//int main()
+//{
+//	int a = 0;
+//	while (a < 10)
+//	{
+//		test();
+//		a++;
+//	}
+//	return 0;
+//
+//}
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d ", a);
+//}
+//int main()
+//{
+//	int a = 0;
+//	while (a < 10)
+//	{
+//		test();
+//		a++;
+//	}
+//	return 0;
+//
+//}
+static int add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a = 10;
+	int b = 30;
+	int sum = add(a, b);
+	printf("%d\n", sum);
+	return 0;
+}
