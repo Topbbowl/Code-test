b52b5fbae5c63d82b8c2fb48bebbea31ddd895b4
大碗 <2541103109@qq.com>
2022-11-03 23:48:30 +0800

    11.3

---
+++ test_11_3/test_11_3/11_3.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	if (m % 5 == 0)
+//		printf("yes\n");
+//	else
+//		printf("no\n");
+//		return 0;
+//}
+//int main()
+//{
+//	if (condition)
+//		return x;
+//	else if (condition)
+//		return y;
+//}
+//int main()
+//{
+//int a = 0;
+//int b = 2;
+//    if (a == 1)
+//  if (b == 2)
+//printf("hehe\n");
+//else
+//printf("haha\n");
+//    return 0;
+//}
